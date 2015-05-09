51d76c2f61a152434a5aadcd689d9ee3a4b1d7b1
Jonathan Grizou <jonathan.grizou@gmail.com>
2015-05-09 16:23:05 +0200

    generate_stl script now automatically detect .scad files and generate them

--- hardware/parts/generate_stl.sh
+++ hardware/parts/generate_stl.sh
@@ -15,42 +15,14 @@ scadArg="\$fn=$fn"
 # create the stl folder if it does not exist
 mkdir -p stl;
 
-# should be automated
-echo "Generating base..."
-openscad -o stl/base.stl base.scad -D $scadArg
+# list all scad file in folder
+scadFileAvailable=$(find . -name "*.scad" -maxdepth 1)
 
-echo "Generating shift_one_side..."
-openscad -o stl/shift_one_side.stl shift_one_side.scad -D $scadArg
-
-echo "Generating U_horn_to_horn..."
-openscad -o stl/U_horn_to_horn.stl U_horn_to_horn.scad -D $scadArg
-
-echo "Generating U_side_to_horn..."
-openscad -o stl/U_side_to_horn.stl U_side_to_horn.scad -D $scadArg
-
-echo "Generating raspberry_base..."
-openscad -o stl/raspberry_base.stl raspberry_base.scad -D $scadArg
-
-echo "Generating circular_raspberry_base..."
-openscad -o stl/circular_raspberry_base.stl circular_raspberry_base.scad -D $scadArg
-
-echo "Generating raspberry_base_with_wheels..."
-openscad -o stl/raspberry_base_with_wheels.stl raspberry_base_with_wheels.scad -D $scadArg
-
-echo "Generating ollo_to_lego_for_wheels..."
-openscad -o stl/ollo_to_lego_for_wheels.stl ollo_to_lego_for_wheels.scad -D $scadArg
-
-echo "Generating passive_wheel..."
-openscad -o stl/passive_wheel.stl passive_wheel.scad -D $scadArg
-
-echo "Generating pen_holder..."
-openscad -o stl/pen_holder.stl pen_holder.scad -D $scadArg
-
-echo "Generating cylinder_head..."
-openscad -o stl/cylinder_head.stl cylinder_head.scad -D $scadArg
-
-echo "Generating lamp_head..."
-openscad -o stl/lamp_head.stl lamp_head.scad -D $scadArg
-
-echo "Generating simple_U..."
-openscad -o stl/simple_U.stl simple_U.scad -D $scadArg
+# generate all filename.scad files and output stls in ./stl/filename.stl
+for i in $scadFileAvailable; do
+  scadFilename=${i#*/}
+  filename=${scadFilename%.scad}
+  stlFilename="stl/$filename.stl"
+  echo "Generating $filename..."
+  openscad -o $stlFilename $scadFilename -D $scadArg
+done
