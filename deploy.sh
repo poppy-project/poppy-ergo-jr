107e3c47f4a13cf31f4e9bef4c074025797e31d5
Jonathan Grizou <jonathan.grizou@gmail.com>
2015-08-09 18:22:17 +0200

    updated script to deploy on github pages

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,29 @@
-# saving work just in case
+#!/bin/bash
+
 git add .
 git commit -m "deploying"
-# pushing to remote master branch
 git push origin master
 
-# on the first run create the gh-pages branch (uncomment next line)
-git branch gh-pages
+##
+git branch -D gh-pages
+git checkout --orphan gh-pages
 
-# creating  local gh-pages branch (on the second and subsequent runs checkout and merge from master)
-# git checkout gh-pages
-# git merge master
+./build.py --full
+mv simulator .tmp
 
-# saving work on local branch
+rm -rf *
+rm .gitignore
+
+mv .tmp/* .
+rm -r .tmp
+
+##
 git add .
-git commit -m "deploying"
+git commit -m "build"
 
-# pushing (and creating if needed) to remote gh-pages branch
+git push origin --delete gh-pages
+
 git push origin gh-pages
 
-# coming back to master branch
+##
 git checkout master
