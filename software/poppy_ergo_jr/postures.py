1026ba3c3c328d1c9a27bdb1dbc871060d7313ac
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-08-11 11:26:46 +0200

    Abstract SimplePosture to pypot (see https://github.com/poppy-project/pypot/commit/65312a3c55a2366f172945dd217ed22d4be3ff1a)

--- software/poppy_ergo_jr/postures.py
+++ software/poppy_ergo_jr/postures.py
@@ -1,20 +1,4 @@
-from pypot.primitive import Primitive
-
-
-class SimplePosture(Primitive):
-    def __init__(self, robot, duration):
-        Primitive.__init__(self, robot)
-
-        self.duration = duration
-
-    def run(self):
-        if not hasattr(self, 'target_position'):
-            raise NotImplementedError('You have to define "target_position" first!')
-
-        for m in self.robot.motors:
-            m.compliant = False
-
-        self.robot.goto_position(self.target_position, self.duration, wait=True)
+from pypot.primitive.utils import SimplePosture
 
 
 class BasePosture(SimplePosture):
