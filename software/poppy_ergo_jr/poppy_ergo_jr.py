8a04d1c5bd8cfd61850a282181603d39061c5821
Théo Segonds <theo.segonds@inria.fr>
2016-01-06 17:07:45 +0100

    indentation fix

--- software/poppy_ergo_jr/poppy_ergo_jr.py
+++ software/poppy_ergo_jr/poppy_ergo_jr.py
@@ -8,10 +8,12 @@ from primitives.dance import Dance
 from primitives.face_tracking import FaceTracking
 from primitives.tracking_feedback import TrackingFeedback
 from primitives.postures import (BasePosture, RestPosture,
-                       CuriousPosture, TetrisPosture,
-                       SafePowerUp)
+                                 CuriousPosture, TetrisPosture,
+                                 SafePowerUp)
+
 
 class PoppyErgoJr(AbstractPoppyCreature):
+
     @classmethod
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(sum, axis=0)
@@ -26,7 +28,6 @@ class PoppyErgoJr(AbstractPoppyCreature):
         robot.attach_primitive(CuriousPosture(robot, 2.), 'curious_posture')
         robot.attach_primitive(TetrisPosture(robot, 2.), 'tetris_posture')
 
-
         if not robot.simulated and hasattr(robot, 'marker_detector'):
             robot.attach_primitive(TrackingFeedback(robot, 25.),
                                    'tracking_feedback')
@@ -39,5 +40,5 @@ class PoppyErgoJr(AbstractPoppyCreature):
 
         if not robot.simulated and hasattr(robot, 'face_tracking'):
             robot.attach_primitive(FaceTracking(robot, 10,
-                                   robot.face_detector),
+                                                robot.face_detector),
                                    'face_tracking')
