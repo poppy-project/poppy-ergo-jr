e122a35731b64c45b4b7c2769d9716fed64660bd
Damien Caselli <damien.caselli@gmail.com>
2016-01-29 12:27:39 +0100

    Remove simulator and gh-pages build code
    gh-pages branch and simulator have been replaced by a dedicated
    repository (https://github.com/poppy-project/poppy-simu).

--- simulator/js/app/stater.js
+++
@@ -1,9 +0,0 @@
-define( ["stats"], function ( Stats, container ) {
-
-  var stater = new Stats();
-  stater.domElement.style.position = 'absolute';
-  stater.domElement.style.top = '0px';
-  stater.domElement.style.zIndex = 100;
-  
-  return stater;
-} );
