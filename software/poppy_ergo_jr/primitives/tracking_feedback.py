cac5ffc741f8ca1269283a6c09db66106ee0850d
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-03-03 16:33:41 +0100

    Change tracking feedback behaviour so the led only blinks when something is detected but does not "hold" the color.

--- software/poppy_ergo_jr/primitives/tracking_feedback.py
+++ software/poppy_ergo_jr/primitives/tracking_feedback.py
@@ -4,14 +4,21 @@ from pypot.primitive import LoopPrimitive
 
 
 class TrackingFeedback(LoopPrimitive):
+    def setup(self):
+        self._old = False
+
     def update(self):
         if not hasattr(self, 'q'):
             self.q = deque([], 6)
 
         self.q.append(len(self.robot.marker_detector.markers) > 0)
+        tracking = any(self.q)
 
-        for m in self.robot.tip:
-            m.led = 'green' if any(self.q) else 'off'
+        if tracking != self._old:
+            for m in self.robot.tip:
+                self.affect_once(m, 'led', 'green' if tracking else 'off')
+
+            self._old = tracking
 
     def teardown(self):
         for m in self.robot.motors:
