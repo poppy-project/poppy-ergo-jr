5a6f42db14023894a5cff4ef0e132f72fab171bd
Pierre Rouanet <pierre.rouanet@gmail.com>
2016-06-16 17:49:31 +0200

    Remove useless primitives.

--- software/poppy_ergo_jr/poppy_ergo_jr.py
+++ software/poppy_ergo_jr/poppy_ergo_jr.py
@@ -5,26 +5,16 @@ from poppy.creatures import AbstractPoppyCreature
 from poppy.creatures.ik import IKChain
 
 from .primitives.dance import Dance
-from .primitives.face_tracking import FaceTracking
-from .primitives.tracking_feedback import TrackingFeedback
 from .primitives.postures import (BasePosture, RestPosture,
                                   CuriousPosture, TetrisPosture,
                                   SafePowerUp)
 
-from .primitives.ball_color_detection import ColorBallDetection
 
 
 class PoppyErgoJr(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
         robot._primitive_manager._filter = partial(sum, axis=0)
-
-        c = IKChain.from_poppy_creature(robot,
-                                        motors=robot.motors,
-                                        passiv=[],
-                                        tip=[0, 0, -0.07])
-
-        robot.chain = c
 
         robot.attach_primitive(SafePowerUp(robot), 'safe_power_up')
 
@@ -35,19 +25,11 @@ class PoppyErgoJr(AbstractPoppyCreature):
         robot.attach_primitive(CuriousPosture(robot, 2.), 'curious_posture')
         robot.attach_primitive(TetrisPosture(robot, 2.), 'tetris_posture')
 
-        if not robot.simulated and hasattr(robot, 'marker_detector'):
-            robot.attach_primitive(TrackingFeedback(robot, 25.),
-                                   'tracking_feedback')
-
         for m in robot.motors:
             m.pid = (4, 2, 0)
             m.torque_limit = 70.
             m.led = 'off'
 
-        if not robot.simulated and hasattr(robot, 'face_tracking'):
-            robot.attach_primitive(FaceTracking(robot, 10,
-                                                robot.face_detector),
-                                   'face_tracking')
 
         robot.attach_primitive(ColorBallDetection(robot),
                                'color_ball_detection')
