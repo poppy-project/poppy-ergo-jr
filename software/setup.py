15d354bb9ae843635f0142147d83a924b57917f1
Yoan Mollard <ymollard@users.noreply.github.com>
2020-06-24 01:33:55 +0200

    Cleaned 2to3 and pypot for py2

--- software/setup.py
+++ software/setup.py
@@ -10,15 +10,11 @@ def version():
     with open('poppy_ergo_jr/_version.py') as f:
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
 
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
-
 setup(name='poppy-ergo-jr',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 3.2.0', 'hampy'],
+      install_requires=['pypot >= 4.0.0', 'hampy'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
@@ -31,5 +27,4 @@ setup(name='poppy-ergo-jr',
       url='https://github.com/poppy-project/poppy-ergo-jr',
       license='GNU GENERAL PUBLIC LICENSE Version 3',
 
-      **extra
       )
