47dd208ab256a526fbd89653b3f4f996ca503a65
Yoan Mollard <ymollard@users.noreply.github.com>
2020-10-23 11:11:10 +0200

    Author update

--- software/setup.py
+++ software/setup.py
@@ -10,6 +10,7 @@ def version():
     with open('poppy_ergo_jr/_version.py') as f:
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
 
+
 setup(name='poppy-ergo-jr',
       version=version(),
       packages=find_packages(),
@@ -21,10 +22,10 @@ setup(name='poppy-ergo-jr',
 
       zip_safe=False,
 
-      author='https://github.com/poppy-project/poppy-ergo-jr/graphs/contributors',
-      author_email='pierre.rouanet@gmail.com',
+      author='Poppy Station',
+      author_email='dev@poppy-station.org',
       description='Poppy Ergo Jr Software Library',
-      url='https://github.com/poppy-project/poppy-ergo-jr',
+      url='https://poppy-project.org',
       license='GNU GENERAL PUBLIC LICENSE Version 3',
 
       )
